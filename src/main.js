3ec247e2d124c016f09d4388eca02d754f4fe6d6
josefraggy <jose.fragoso@positivecompliance.com>
2018-12-29 15:46:22 -0600

    Primera Prueba con Vuex

--- src/main.js
+++ src/main.js
@@ -56,3 +56,20 @@ new Vue({
   router,
   render: h => h(Layout)
 }).$mount('#app')
+
+// Vuex
+// Make sure to call Vue.use(Vuex) first if using a module system
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+store.commit('increment')
+
+console.log(store.state.count) // -> 1
